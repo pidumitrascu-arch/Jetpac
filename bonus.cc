5926f518cc7033473b7694ee6bb2e3703735a129
ibarguenfe-alt <ibarguenfe@esat-alumni.com>
2026-02-24 20:06:34 +0100

    Add files via upload

--- bonus.cc
+++ bonus.cc
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <time.h>
 
-
+#define num_bonus 5 //cantidad de bonus items
 
 double current_time, last_time;
 
@@ -18,67 +18,142 @@ const int scale = 3;
 const unsigned int screenX = 256 * scale;
 const unsigned int screenY = 192 * scale;
 
-int fps = 60;
+const int wX = 800, wY = 600;
 
+int fps = 60;
 
 
 struct Bonus //Struct Bonus items
 {
     esat::SpriteTransform BonusTransform;
-   
-    int BonusDirrection;
+    float x, y;
+    int BonusDirection;
     float BonusSpeed;
-    esat::SpriteHandle sprite[3];
+    bool active = false;
+    esat::SpriteHandle sprite;
 };
-Bonus bonus;
-struct bonus{
-    esat::Sprite
-}
-void Bonus() //Aquí imprimo los sprites de los diferentes Bonus que hay.
+
+Bonus* bonus = nullptr;
+
+void SpritesBonus() //Aquí imprimo los sprites de los diferentes Bonus que hay.
 {
-    sprite[0]= esat::SpriteFromFile("./Recursos/Diamante.png");
+    bonus[0].sprite = esat::SpriteFromFile("./Recursos/Diamond.png");
 
-    sprite[1]= esat::SpriteFromFile("./Recursos/Geoda.png");
+    bonus[1].sprite = esat::SpriteFromFile("./Recursos/Geode.png");
     
-    sprite[2]= esat::SpriteFromFile("./Recursos/Lingote.png");
+    bonus[2].sprite = esat::SpriteFromFile("./Recursos/Dablooon.png");
 
-    //A continuación haremos que aparezcan en pantalla en sitios aleatorios y en caso que no haya suelo, q caiga dentro del rectángulo del suelo, para que no se pierda el Bonus.
+    bonus[3].sprite = esat::SpriteFromFile("./Recursos/Triangle.png");
 
-    int randomX = rand() % (screenX - 50); // Genera una posición X aleatoria dentro de los límites de la pantalla
-    int randomY = rand() % (screenY - 50); // Genera una posición Y aleatoria dentro de los límites de la pantalla
-    
-
-    
+    bonus[4].sprite = esat::SpriteFromFile("./Recursos/Bomb.png");   
 }
 
+void InitBonus(Bonus* bn) //posicion y estado inicial de los bonus items
+{
+    bn->active = false;
+    bn->x = 0.0f;
+    bn->y = 0.0f;
+    bn->BonusSpeed = 0.0f;
 
+}
 
+void SpawnBonus(int index) //posicion y estado de los bonus items cuando aparecen
+{
+    Bonus* bn = bonus + index;
+
+    bn->active = true;
+    bn->x = rand() % (wX - 50); //Genera una posición X aleatoria dentro de los límites de la pantalla
+    bn->y = 0.0f;
+    bn->BonusSpeed = 2.0f + rand()%3;
+}
+
+bool OneBonusAtATime() //para que aparezca solo uno a la vez
+{
+    for(int i = 0; i < num_bonus; ++i){
+        if(bonus[i].active) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void BonusMov()//animacion
+{
+    for(int i = 0; i < num_bonus; ++i){
+
+        Bonus* bn = bonus + i;
+
+        if(bn->active){
+            bn->y += bn->BonusSpeed;
+
+            if(bn->y > wY){
+                bn->active = false;
+            }
+
+            if(bn->y > 510){//si toca el suelo
+                bn->y = 510;
+            }
+        }
+    }
+}
+
+void DrawBonus()
+{
+    for(int i = 0; i < num_bonus; ++i){
+
+        Bonus* bn = bonus + i;
+
+        if(bn->active){
+            esat::DrawSprite(bn->sprite, bn->x, bn->y);
+        }
+    }
+}
+
+void RunBonus()
+{
+    static double last_spawn = 0; //Con static: la variable conserva su valor entre llamadas.
+
+    if(current_time - last_spawn > 8000 && !OneBonusAtATime()){
+    int random_bonus = rand() % num_bonus;
+    SpawnBonus(random_bonus);
+    last_spawn = current_time;}
+
+    BonusMov();
+    DrawBonus();
+}
 
 int esat::main(int argc, char **argv)
 {
 
     srand(time(NULL));
 
+    bonus = (Bonus*)malloc(sizeof(Bonus) * num_bonus);
+
+    for(int i = 0; i < num_bonus; ++i){
+        InitBonus(&bonus[i]);
+    }
+    
     esat::WindowInit(screenX, screenY);
     esat::WindowSetMouseVisibility(true);
 
+    SpritesBonus();
+
     while (esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape))
     {
-
-        last_time = esat::Time();
-        esat::DrawBegin();
-        esat::DrawClear(0, 0, 0);
-
-
-
-        esat::DrawEnd();
-
         // Control fps
         do {
 
             current_time = esat::Time();
 
         } while ((current_time - last_time) <= 1000.0 / fps);
+
+        last_time = esat::Time();
+        esat::DrawBegin();
+        esat::DrawClear(255, 255, 255);
+
+        RunBonus();
+
+        esat::DrawEnd();
 
         esat::WindowFrame();
     }
